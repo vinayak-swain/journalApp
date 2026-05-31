43304032735f7099a4d41fa9d294ccafd559e78a
Vinayak Swain <vinayakswain49@gmail.com>
2026-06-01 00:28:19 +0530

    lec 17

--- src/main/java/com/vinayak/journalApp/service/JournalEntryService.java
+++ src/main/java/com/vinayak/journalApp/service/JournalEntryService.java
@@ -7,6 +7,8 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.Optional;
@@ -20,12 +22,19 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName) {
-        User user = userService.findByUserName(userName);
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry saved = journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        userService.saveEntry(user);
+        try {
+            User user = userService.findByUserName(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            user.setUserName(null);
+            userService.saveEntry(user);
+        }catch (Exception e){
+            System.out.println(e);
+            throw new RuntimeException("Error saving JournalEntry");
+        }
     }
     public void saveEntry(JournalEntry journalEntry) {
         journalEntryRepository.save(journalEntry);
